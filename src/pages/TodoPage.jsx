379d07d4fa84f01f6ac792811c386493f4ce11d5
evanyang0612 <blacksheet@gmail.com>
2022-11-19 13:38:42 +0800

    delete and footer updated

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,5 +1,6 @@
 import { Footer, Header, TodoCollection, TodoInput } from 'components';
-
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 const dummyTodos = [
   {
     title: 'Learn react-router',
@@ -24,13 +25,94 @@ const dummyTodos = [
 ];
 
 const TodoPage = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [todos, setTodos] = useState(dummyTodos);
+
+  function handleInputChange(e) {
+    setInputValue(e.target.value);
+  }
+
+  function handleAddTodo() {
+    if (inputValue.length === 0) return;
+    setTodos((prevTodos) => {
+      return [
+        {
+          id: uuidv4(),
+          title: inputValue,
+          isDone: false,
+        },
+        ...prevTodos,
+      ];
+    });
+
+    setInputValue('');
+  }
+
+  function handleToggleDone(id) {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            isDone: !todo.isDone,
+          };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function handleEditMode({ id, isEdit }) {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            isEdit,
+          };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function handleSave({ id, title }) {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            title,
+            isEdit: false,
+          };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function handleDelete(id) {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  }
+
   return (
     <div>
       TodoPage
       <Header />
-      <TodoInput />
-      <TodoCollection />
-      <Footer />
+      <TodoInput
+        inputValue={inputValue}
+        onInputChange={handleInputChange}
+        onAddTodo={handleAddTodo}
+        onKeyDown={handleAddTodo}
+      />
+      <TodoCollection
+        todos={todos}
+        onToggleDone={handleToggleDone}
+        onChangeMode={handleEditMode}
+        onSave={handleSave}
+        onDelete={handleDelete}
+      />
+      <Footer todos={todos}/>
     </div>
   );
 };
